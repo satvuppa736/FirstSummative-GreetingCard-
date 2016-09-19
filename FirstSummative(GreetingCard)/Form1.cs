e42f411e242fa74cf516873db35875e8715e9fbf
satvuppa736 <satvuppa736@ed.amdsb.ca>
2016-09-19 12:42:15 -0400

    First Commit

--- FirstSummative(GreetingCard)/Form1.cs
+++ FirstSummative(GreetingCard)/Form1.cs
@@ -16,5 +16,10 @@ namespace FirstSummative_GreetingCard_
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //first Commit
+        }
     }
 }
